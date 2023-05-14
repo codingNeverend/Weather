f2acef1ad0eb48eb56d39f7fa1bfd1fc44af4141
codingNeverend <neverend.coding@gmail.com>
2023-05-14 22:21:50 +0200

    Commit

---
+++ script.js
@@ -0,0 +1,50 @@
+let url;
+let townElement = document.querySelector(".town");
+let temperatureElement = document.querySelector("#temperature_label");
+let newTown = document.querySelector("#changeVille");
+let container = document.querySelector(".container");
+let logo = document.querySelector('#logo');
+let menu = document.querySelector('.burger'); 
+let menuTown = document.querySelector('.changeTown'); 
+let card = document.querySelector('.card');
+
+refreshWeather("Paris");
+
+async function refreshWeather(town) {
+  url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    town +
+    "&appid=0495a3371f7d23fcb66cb1628b1d1962&units=metric";
+    
+  axios
+    .get(url)
+    .then((donnees) => {
+      townElement.textContent = town;
+      temperatureElement.textContent = donnees.data.main.temp;
+      if (donnees.data.weather[0].main == "Clouds") {
+        container.style.backgroundImage =
+          "url('images/bgclouds.jpg')";
+          logo.src = "images/cloudy.png";
+          card.style.backgroundColor = "lightgrey";
+        } else if (donnees.data.weather[0].main == "Clear") {
+        container.style.backgroundImage =
+          "url('images/blueSky.jpeg')";
+          logo.src = "images/sun.png";
+          card.style.backgroundColor = "blue";
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+submit.addEventListener("click", () => {
+  newTown = document.querySelector("#changeVille").value;
+  refreshWeather(newTown);
+  document.querySelector("#changeVille").value = "";
+  menuTown.classList.toggle('changeTownAnime');
+});
+
+menu.addEventListener("click", () => {
+  menuTown.classList.toggle('changeTownAnime');
+});
